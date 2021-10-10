3857fe499a074489ac68727760138e042993b67e
Edwin Benalcázar <softwareywebsoluciones@gmail.com>
2021-10-10 11:44:16 -0500

    feat: Add Context and Reducer to manage Auth

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { AuthContext } from './auth/AuthContext'
+import { authReducer } from './auth/authReducer'
 import { AppRouter } from './routers/AppRouter'
 export const HeroesApp = () => {
+    
+    const init = ()=>{
+        return JSON.parse(localStorage.getItem('user')) || { logged: false };
+    }
+    const [user, dispatch] = useReducer(authReducer, {}, init);
+
     return (
-        <AppRouter />
+        <AuthContext.Provider value={{user, dispatch}}>
+            <AppRouter />
+        </AuthContext.Provider>
     )
 }
